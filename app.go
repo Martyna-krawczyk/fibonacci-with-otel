e1f4349b1d02981a6ffcbc890ba2a746913daf82
Martyna Krawczyk <martynakraw@gmail.com>
2023-05-03 14:43:59 +1000

    instrument code with open-telemetry

--- app.go
+++ app.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type App struct {
@@ -12,32 +17,59 @@ type App struct {
 	l *log.Logger
 }
 
+// name is the Tracer name used to identify this instrumentation library.
+const name = "fibonacci-with-otel"
+
 func NewApp(r io.Reader, l *log.Logger) *App {
 	return &App{r: r, l: l}
 }
 
 func (a *App) Run(ctx context.Context) error {
 	for {
-		n, err := a.Poll(ctx)
+		// Each execution of the run loop, we should get a new "root" span and context
+		// starts a new OpenTelemetry span for the Write() method with the name "Poll"
+		// The Start() method returns a new context and a trace.Span object, which
+		// is used to end the span.
+		newCtx, span := otel.Tracer(name).Start(ctx, "Run")
+
+		n, err := a.Poll(newCtx)
 		if err != nil {
+			span.End()
 			return err
 		}
-		a.Write(ctx, n)
+		a.Write(newCtx, n)
+		span.End()
 	}
 }
 
 func (a *App) Poll(ctx context.Context) (uint, error) {
+	_, span := otel.Tracer(name).Start(ctx, "Poll")
+	defer span.End()
+
 	a.l.Print("What fibonacci no would you like to know?")
 
-	// reading a single integer value from the input source and returning it to 
+	// reading a single integer value from the input source and returning it to
 	// the calling function along with any errors encountered during the reading process
 	var n uint
 	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+
+	// Store n as a string to not overflow an int64.
+	nStr := strconv.FormatUint(uint64(n), 10)
+	span.SetAttributes(attribute.String("request.n", nStr))
 	return n, err
 }
 
 func (a *App) Write(ctx context.Context, n uint) {
-	f, err := Fibonacci(n)
+	var span trace.Span
+	ctx, span = otel.Tracer(name).Start(ctx, "Write")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (uint64, error) {
+		_, span := otel.Tracer(name).Start(ctx, "Fibonacci")
+		defer span.End()
+		return Fibonacci(n)
+	}(ctx)
+
 	if err != nil {
 		a.l.Printf("fibonacci(%d) = %v\n", n, err)
 	} else {
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module fibonacci-with-otel
 
 go 1.19
+
+require (
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.15.1 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.15.1 // indirect
+	go.opentelemetry.io/otel/sdk v1.15.1 // indirect
+	go.opentelemetry.io/otel/trace v1.15.1 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
